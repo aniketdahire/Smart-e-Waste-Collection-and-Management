889e0221469f3de23640804d10adc5f4cb65cbfd
Aniket Dahire <aniketdahire476@gmail.com>
2025-11-29 02:09:42 +0530

    Initial project setup

---
+++ src/test/java/com/Smart/e_Waste/Collection/Management/System/Smart/e_Waste/Collection/Management/System/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Smart.e_Waste.Collection.Management.System.Smart.e_Waste.Collection.Management.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
